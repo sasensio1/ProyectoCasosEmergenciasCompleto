f734fb0c7be846e1cecd5a8a7ee3def92e603575
sasensioDTUser <sasensio@10.0.2.15>
2017-01-13 09:51:32 +0100

    sasensio:Creación de jsp  de Historic Batch

--- casosEmergenciasBulkApi/src/main/java/com/casosemergencias/controller/BatchController.java
+++ casosEmergenciasBulkApi/src/main/java/com/casosemergencias/controller/BatchController.java
@@ -1,14 +1,18 @@
 package com.casosemergencias.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.casosemergencias.logic.BatchService;
 
 @Controller
 public class BatchController {
+	
+	final static Logger logger = Logger.getLogger(BatchController.class);
 		
 	@Autowired
 	private BatchService batchService;
@@ -21,5 +25,23 @@ public class BatchController {
 	@RequestMapping(value = "/updateHerokuFieldLabelTable", method = RequestMethod.GET)
 	public void fieldLabelHome() {
 		batchService.updateHerokuFieldLabelTable();
+	}
+	
+	/**
+	 * Metodo que recupera una lista con todos los historicos que hay creados en BBDD y los muestra en la pantalla homeHistoricBatchPage.jsp
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/private/homeHistoricBatch", method = RequestMethod.GET)
+	public ModelAndView goHomeHistoricBatch() {
+		
+		logger.info("--- Inicio -- listadoHistoricBatch ---");
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("private/homeHistoricBatchPage");
+	
+		logger.info("--- Fin -- listadoHistoricBatch ---");
+				
+		return model;
 	}
 }
