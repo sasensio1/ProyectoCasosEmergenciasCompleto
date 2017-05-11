757668adb243a0b5d503d67b56b1f7fc69ee4946
PrEmer <maserasen@gmail.com>
2017-05-11 23:35:55 +0200

    Update AccountUpdaterBatch.java

--- casosEmergenciasBulkApi/src/main/java/com/casosemergencias/batch/AccountUpdaterBatch.java
+++ casosEmergenciasBulkApi/src/main/java/com/casosemergencias/batch/AccountUpdaterBatch.java
@@ -20,10 +20,9 @@ public class AccountUpdaterBatch {
 	
 	public void updateAccountInfo() {
 		LOGGER.trace("Comienzo del proceso de actualizacion de Account de SalesForce a la base de datos de Heroku");
-		Date processEndDate = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(processEndDate);
-		cal.add(Calendar.MINUTE, -30);
+		Date yesterday = Utils.substractDaysToDate(new Date(), 1);
+		Date processStartDate = Utils.setHourInDate(yesterday, 0, 0, 0, 0);
+		Date processEndDate = Utils.setHourInDate(yesterday, 23, 59, 59, 999);
 		Date processStartDate = cal.getTime();
 		//FIXME: Comprobar zona horaria.
 		restApiInvokerBatch.setProcessStartDate(processStartDate);
